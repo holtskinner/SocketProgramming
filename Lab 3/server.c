d1ce7ee5cb0e7ef5d007375fc16ddaf07a3c45c5
Holt Skinner <HoltWasHere@gmail.com>
2016-11-01 09:56:03 -0500

    Created initial server.c

--- Lab 3/server.c
+++ Lab 3/server.c
@@ -18,6 +18,29 @@
 
 int main() {
 
+  char server_message[256] = "Hello World!";
+
+  //create server socket
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  //Address for socket
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(PORT);
+  server_address.sin_addr.s_addr = INADDR_ANY; //local
+
+  //Bind to recieve response
+  bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+  //Wait for response
+  listen(server_socket, 1); //Allow 1 user at a time
+
+  //Get response from client
+  int client_socket = accept(server_socket, NULL, NULL);//Last two parameters left as NULL becuase it's all local
+  //send message
+  send(client_socket, server_message, sizeof(server_message), 0);
+
+  close(server_socket);
   
   return 0;
 }
